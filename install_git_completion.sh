53ef42ed62e879927d91380033c6e5145130f6f7
linghutf <xurui788@gmail.com>
2017-10-14 11:21:18 +0800

    add git alias in script

--- install_git_completion.sh
+++ install_git_completion.sh
@@ -11,3 +11,11 @@ echo "if [ -f ~/git-completion.bash ];then\n\
     source ~/git-completion.bash\n\
 fi\n" >> ~/.bashrc
 
+git config --global alias.co checkout
+
+git config --global alias.br branch
+
+git config --global alias.ci commit
+
+git config --global alias.st status
+
